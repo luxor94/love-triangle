b03df0c858942ad4387dc7d7bfa372336ceb07c6
luxor94 <47752490+luxor94@users.noreply.github.com>
2019-03-03 11:00:38 +0300

    new code
    new code

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+  let count=0;
+  for(let i=0;i<preferences.length;i++){
+   let lover = preferences[i];
+   if(lover==i+1) continue;
+   let lover2=preferences[preferences[i]-1];
+   if(preferences[lover2-1]==i+1){
+     count++;
+     delete(preferences[i]);
+   }
+  }
+  return count;
 };
